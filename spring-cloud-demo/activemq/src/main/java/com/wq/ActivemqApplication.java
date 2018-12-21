b81472f70d627ecfe1deeb544304b0f3e1a438b4
wengqian <18767126010@sina.cn>
2018-12-21 10:37:57 +0800

    activemq

---
+++ spring-cloud-demo/activemq/src/main/java/com/wq/ActivemqApplication.java
@@ -0,0 +1,14 @@
+package com.wq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqApplication.class, args);
+	}
+
+}
+
